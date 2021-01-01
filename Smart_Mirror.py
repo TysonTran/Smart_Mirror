310496118e5e8de9f43de388ee6413c195b58e5c
Tyson Tran <tysontran2@gmail.com>
2020-12-31 20:26:49 -0800

    Found forecast and description of weather

--- Smart_Mirror.py
+++ Smart_Mirror.py
@@ -11,17 +11,22 @@ response = requests.get(full_link)
 
 #Turns JSON format into Python format
 x = response.json()
+print(x)
 
-y = x["main"]
-current_temp = y["temp"]
-print(kelvin_to_farenheit(current_temp))
+temps = x["main"]
+current_temp = temps["temp"] #tells weather in Farenheit
 
-class Weather(Frame):
+weather = x["weather"][0]
+forecast = weather["main"] #tells weather rain, cloud, or weather
+forecast_desc = weather["description"]
+
+class Weather_Widget(Frame):
 	def __init__(self, parent): #Parent is where frame widget is created
 		super().__init__(self, parent, bg = 'black') #Uses init from Frame
 		self.temp = ''
+		self.forecast = ''
 
 
 	
 	def kelvin_to_farenheit(k):
-	return round((k*(9/5)) - 459.67, 2)
+		return round((k*(9/5)) - 459.67, 2)
